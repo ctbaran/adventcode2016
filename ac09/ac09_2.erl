63d8edde9c906f9183112fe15612206238bca2ef
Christian Baran <xn.baran@gmail.com>
2016-12-09 09:06:34 -0500

    Added code to flush buffer

--- ac09/ac09_2.erl
+++ ac09/ac09_2.erl
@@ -4,7 +4,7 @@
 solve(File) ->
 	{ok, FH} = file:open(File, [read]),
 	[Lines] = read(FH,[]),
-	length( lists:reverse( expand_input(Lines, {0, 0}, [], []) ) ).
+	expand_input(Lines, {0, 0}, 0, []).
 
 read(File, Lines) ->
 	case file:read_line(File) of
@@ -20,18 +20,19 @@ read_expand(Instruction) ->
 	[N, Repeats] = string:tokens(Instruction, "x"),
 	{list_to_integer(N), list_to_integer(Repeats)}.
 
-expand_input([], {0, 0}, Expanded, _Buffer) -> Expanded;
-expand_input([], {0, Repeat}, Expanded, Buffer) -> 
-	expand_input( lists:reverse(string:copies(Buffer, Repeat)) ++ [], {0, 0}, Expanded, []);
-expand_input([40|Rest], {0, 0}, Expanded, Buffer) ->
+expand_input([], {0, 0}, Length, Buffer) -> Length;
+expand_input([], {0, Repeat}, Length, Buffer) -> 
+	Expanded = string:copies(Buffer, Repeat),
+	expand_input( lists:reverse(Expanded), {0, 0}, Length, []);
+expand_input([40|Rest], {0, 0}, Length, Buffer) ->
 	UpToLeft = lists:takewhile(fun(X) -> X /= 41 end, Rest),
 	ExpandIn = read_expand(UpToLeft),
 	RemoveMarker = tl( lists:subtract(Rest, UpToLeft) ),
-	expand_input(RemoveMarker, ExpandIn, Expanded, Buffer);
-expand_input([Char|Rest], {0, 0}, Expanded, Buffer) ->
-	expand_input(Rest, {0, 0}, [Char] ++ Expanded, Buffer);
-expand_input(String, {0, Repeat}, Expanded, Buffer) ->
+	expand_input(RemoveMarker, ExpandIn, Length, Buffer);
+expand_input([Char|Rest], {0, 0}, Length, Buffer) ->
+	expand_input(Rest, {0, 0}, Length + 1, Buffer);
+expand_input(String, {0, Repeat}, Length, Buffer) ->
 	EmptyBuffer = string:copies(Buffer, Repeat),
-	expand_input(lists:reverse(EmptyBuffer) ++ String, {0, 0}, Expanded, []);
-expand_input([Char|Rest], {X, Repeat}, Expanded, Buffer) ->
-	expand_input(Rest, {X-1, Repeat}, Expanded, [Char|Buffer]).
+	expand_input(lists:reverse(EmptyBuffer) ++ String, {0, 0}, Length, []);
+expand_input([Char|Rest], {X, Repeat}, Length, Buffer) ->
+	expand_input(Rest, {X-1, Repeat}, Length, [Char|Buffer]).
