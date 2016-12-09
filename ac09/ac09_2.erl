6e747b3af658993f3aef4d912bcb4a106cb02080
Christian Baran <xn.baran@gmail.com>
2016-12-09 09:17:17 -0500

    Cut down a bit on the big reversals

--- ac09/ac09_2.erl
+++ ac09/ac09_2.erl
@@ -20,19 +20,19 @@ read_expand(Instruction) ->
 	[N, Repeats] = string:tokens(Instruction, "x"),
 	{list_to_integer(N), list_to_integer(Repeats)}.
 
-expand_input([], {0, 0}, Length, Buffer) -> Length;
+expand_input([], {0, 0}, Length, _Buffer) -> Length;
 expand_input([], {0, Repeat}, Length, Buffer) -> 
-	Expanded = string:copies(Buffer, Repeat),
-	expand_input( lists:reverse(Expanded), {0, 0}, Length, []);
+	EmptyBuffer = string:copies(lists:reverse(Buffer), Repeat),
+	expand_input(EmptyBuffer, {0, 0}, Length, []);
 expand_input([40|Rest], {0, 0}, Length, Buffer) ->
 	UpToLeft = lists:takewhile(fun(X) -> X /= 41 end, Rest),
 	ExpandIn = read_expand(UpToLeft),
 	RemoveMarker = tl( lists:subtract(Rest, UpToLeft) ),
 	expand_input(RemoveMarker, ExpandIn, Length, Buffer);
-expand_input([Char|Rest], {0, 0}, Length, Buffer) ->
+expand_input([_Char|Rest], {0, 0}, Length, Buffer) ->
 	expand_input(Rest, {0, 0}, Length + 1, Buffer);
 expand_input(String, {0, Repeat}, Length, Buffer) ->
-	EmptyBuffer = string:copies(Buffer, Repeat),
-	expand_input(lists:reverse(EmptyBuffer) ++ String, {0, 0}, Length, []);
+	EmptyBuffer = string:copies(lists:reverse(Buffer), Repeat),
+	expand_input(EmptyBuffer ++ String, {0, 0}, Length, []);
 expand_input([Char|Rest], {X, Repeat}, Length, Buffer) ->
 	expand_input(Rest, {X-1, Repeat}, Length, [Char|Buffer]).
