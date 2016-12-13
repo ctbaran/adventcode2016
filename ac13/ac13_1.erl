89cbd9c0f87c06c1d278fb88a7b59fbe6d654954
Christian Baran <xn.baran@gmail.com>
2016-12-13 09:00:58 -0500

    Day 13

---
+++ ac13/ac13_1.erl
@@ -0,0 +1,52 @@
+-module(ac13_1).
+-export([solve/3]).
+
+solve(XTarget, YTarget, FavNum) ->
+	Maze = initialize_maze(1000,1000,FavNum),
+	find_target(Maze, {{1,1}, 0}, {XTarget,YTarget}, queue:new()).
+
+compute(X, Y, FavNum) ->
+	Num = (X*X) + (3*X) + (2*X*Y) + Y + (Y*Y),
+	Bin = integer_to_list( Num + FavNum, 2),
+	case bits(Bin, 0) rem 2 == 0 of
+		true -> open;
+		false -> closed
+	end.
+
+bits([], Ones) -> Ones;
+bits([49|Rest], Ones) -> bits(Rest, Ones+1);
+bits([_|Rest], Ones) -> bits(Rest, Ones).
+
+
+initialize_maze(XMax, YMax, FavNum) ->
+	Maze = ets:new(maze, []),
+	lists:foreach( fun({X,Y}) ->
+					ets:insert(Maze, {{X,Y}, compute(X,Y, FavNum), unvisited})
+				 end,
+				 [ {X,Y} || X<-lists:seq(0,XMax), Y<-lists:seq(0,YMax) ]),
+	Maze.
+
+% tail recursive BF of state space
+find_target(_Maze, {TargetPosition, Distance}, TargetPosition, _ToVisit) -> Distance;
+find_target(Maze, {Position, Distance}, TargetPosition, ToVisit) ->
+	ets:update_element(Maze, Position, {3,visited}),
+	Adjacent = get_adjacent(Position),
+	UpdatedToVisit = lists:foldl( fun(NewPosition, Q) ->
+						case ets:lookup(Maze, NewPosition) of
+							[{NewPosition, closed, _}] ->
+								Q;
+							[{NewPosition, open, visited}] ->
+								Q;
+							[{NewPosition, open, unvisited}] ->
+								queue:in({NewPosition,Distance+1}, Q)
+						end
+				   end,
+				   ToVisit,
+				   Adjacent),
+	{{value, NextPosition}, NextToVisit} = queue:out(UpdatedToVisit),
+	find_target(Maze, NextPosition, TargetPosition, NextToVisit).
+
+get_adjacent({0,0}) -> [{0,1},{1,0}];
+get_adjacent({X,0}) -> [{X,1},{X-1,0},{X+1,0}];
+get_adjacent({0,Y}) -> [{1,Y},{0,Y-1},{0,Y+1}];
+get_adjacent({X,Y}) -> [{X-1,Y},{X,Y-1},{X+1,Y},{X,Y+1}].
